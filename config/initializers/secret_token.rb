b43272ae8127464dd8b4ebfb7e15dc628e1712fd
Jim Knight <jim.knight@lavatech.com>
2012-01-11 20:27:33 -0500

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Moonshine::Application.config.secret_token = 'd7e7aa088eef37402ea93ebc554b7c30dcda812ccf6e31f16a0b3ca406465f0003b2a3081e360eb932092814fb82e21d310e28a64855d37d6b1fabb425798e5a'
